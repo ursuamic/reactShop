6f3ee89de96cbbc58a78647e79ae1173325c49ef
Björn Wänn <>
2020-08-12 22:24:46 +0200

    cart-dropdown component with added cart-item functionality

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomBotton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={ `${ isGoogleSignIn ? 'google-sign-in' : '' } custom-button` }  {...otherProps}>
+const CustomBotton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+  <button className={ `${ inverted ? 'inverted' : '' } ${ isGoogleSignIn ? 'google-sign-in' : '' } custom-button` }  {...otherProps}>
   {children}
   </button>
 );
