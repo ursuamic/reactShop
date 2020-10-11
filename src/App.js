b8266ebadf759e780adb0dcca03a6580397087d5
Björn Wänn <>
2020-10-11 20:39:52 +0200

    directory and shop component update

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/shop/' component={ShopPage} />
+          <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckOutPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
         </Switch>
